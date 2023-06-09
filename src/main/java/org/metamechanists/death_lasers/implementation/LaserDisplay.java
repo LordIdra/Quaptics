ec10fcfbb60ebe7d3df68a01f92f7677786e6670
MetaMechanists <percy.baker.0@gmail.com>
2023-06-09 16:49:52 +0100

    BLock display + actually register initial display

--- src/main/java/org/metamechanists/death_lasers/implementation/LaserDisplay.java
+++ src/main/java/org/metamechanists/death_lasers/implementation/LaserDisplay.java
@@ -1,8 +1,11 @@
 package org.metamechanists.death_lasers.implementation;
 
 import dev.sefiraat.sefilib.entity.display.DisplayGroup;
+import dev.sefiraat.sefilib.entity.display.builders.BlockDisplayBuilder;
 import dev.sefiraat.sefilib.misc.TransformationBuilder;
 import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.util.Vector;
 
 public class LaserDisplay {
     private static final String MAIN_DISPLAY_NAME = "MAIN";
@@ -11,6 +14,10 @@ public class LaserDisplay {
 
     public LaserDisplay(Location location) {
         group = new DisplayGroup(location);
+        group.addDisplay(MAIN_DISPLAY_NAME, new BlockDisplayBuilder()
+                .setGroupParentOffset(new Vector(0, 1, stage))
+                .setBlockData(Material.RED_CONCRETE.createBlockData())
+                .build(group));
         update();
     }
 
