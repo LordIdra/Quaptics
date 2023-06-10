4253c29a1c9f1b0e1bdc342b9b85b4b8afc961e3
MetaMechanists <percy.baker.0@gmail.com>
2023-06-10 21:56:16 +0100

    please

--- src/main/java/org/metamechanists/death_lasers/lasers/ticker/ticker/LinearTimeTicker.java
+++ src/main/java/org/metamechanists/death_lasers/lasers/ticker/ticker/LinearTimeTicker.java
@@ -2,7 +2,9 @@ package org.metamechanists.death_lasers.lasers.ticker.ticker;
 
 import dev.sefiraat.sefilib.entity.display.builders.BlockDisplayBuilder;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.BlockDisplay;
+import org.bukkit.entity.Display;
 import org.bukkit.util.Vector;
 import org.metamechanists.death_lasers.DEATH_LASERS;
 
@@ -17,7 +19,10 @@ public class LinearTimeTicker implements LaserBlockDisplayTicker {
         velocity = target.toVector()
                 .subtract(source.toVector())
                 .multiply(1.0/lifespanTicks);
-        this.display = displayBuilder
+        this.display = new BlockDisplayBuilder()
+                .setBrightness(new Display.Brightness(4, 0))
+                .setBlockData(Material.RED_CONCRETE.createBlockData())
+                .setGroupParentOffset(new Vector())
                 .setLocation(source)
                 .setDisplayHeight(0.1F)
                 .setDisplayWidth(0.1F)
