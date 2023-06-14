ac2a54b38e1ae9aa248e4d4e1b7f9f7d41028c07
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2023-06-14 17:11:27 -0500

    Use beta things :D

--- src/main/java/org/metamechanists/death_lasers/utils/PersistentDataLocation.java
+++ src/main/java/org/metamechanists/death_lasers/utils/PersistentDataLocation.java
@@ -1,12 +1,14 @@
 package org.metamechanists.death_lasers.utils;
 
+import com.google.common.io.ByteArrayDataInput;
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
 import org.bukkit.Location;
 import org.bukkit.persistence.PersistentDataAdapterContext;
 import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 import org.metamechanists.death_lasers.DEATH_LASERS;
 
-import java.nio.ByteBuffer;
 import java.util.UUID;
 
 public class PersistentDataLocation implements PersistentDataType<byte[], Location> {
@@ -22,24 +24,24 @@ public class PersistentDataLocation implements PersistentDataType<byte[], Locati
 
     @Override
     public byte @NotNull [] toPrimitive(@NotNull Location complex, @NotNull PersistentDataAdapterContext persistentDataAdapterContext) {
-        ByteBuffer buffer = ByteBuffer.wrap(new byte[48]);
-        buffer.putLong(complex.getWorld().getUID().getMostSignificantBits());
-        buffer.putLong(complex.getWorld().getUID().getLeastSignificantBits());
-        buffer.putDouble(complex.getX());
-        buffer.putDouble(complex.getY());
-        buffer.putDouble(complex.getZ());
-        return buffer.array();
+        final ByteArrayDataOutput output = ByteStreams.newDataOutput();
+        output.writeLong(complex.getWorld().getUID().getMostSignificantBits());
+        output.writeLong(complex.getWorld().getUID().getLeastSignificantBits());
+        output.writeDouble(complex.getX());
+        output.writeDouble(complex.getY());
+        output.writeDouble(complex.getZ());
+        return output.toByteArray();
     }
 
     @NotNull
     @Override
     public Location fromPrimitive(byte @NotNull [] primitive, @NotNull PersistentDataAdapterContext persistentDataAdapterContext) {
-        ByteBuffer buffer = ByteBuffer.wrap(primitive);
-        final long msb = buffer.getLong();
-        final long lsb = buffer.getLong();
-        final double x = buffer.getDouble();
-        final double y = buffer.getDouble();
-        final double z = buffer.getDouble();
+        final ByteArrayDataInput input = ByteStreams.newDataInput(primitive);
+        final long msb = input.readLong();
+        final long lsb = input.readLong();
+        final double x = input.readDouble();
+        final double y = input.readDouble();
+        final double z = input.readDouble();
         final UUID uuid = new UUID(msb, lsb);
         return new Location(DEATH_LASERS.getInstance().getServer().getWorld(uuid), x, y, z);
     }
