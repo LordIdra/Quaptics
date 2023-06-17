7fa47fd7ae7e0033328ead1d4e5d252d3c1a0ee2
MetaMechanists <percy.baker.0@gmail.com>
2023-06-17 22:41:20 +0100

    argh

--- src/main/java/org/metamechanists/death_lasers/utils/DisplayUtils.java
+++ src/main/java/org/metamechanists/death_lasers/utils/DisplayUtils.java
@@ -43,7 +43,7 @@ public class DisplayUtils {
         final float horizontalRotation = getHorizontalRotation(from, to);
         final Vector directionVectorInOnlyHorizontalPlane = new Vector(1, 0, 0).rotateAroundY(horizontalRotation);
         float verticalRotation = directionVectorInOnlyHorizontalPlane.angle(displacement);
-        //if (displacement.getY() > 0) { verticalRotation *= -1; }
+        //if (displacement.getY()/displacement.getX() > 0) { verticalRotation *= -1; }
         return verticalRotation;
     }
 
@@ -69,7 +69,7 @@ public class DisplayUtils {
 
         return new Matrix4f()
                 .rotateY(horizontalRotation)
-                .rotate(verticalRotation, (float)Math.sin(horizontalRotation), 0, (float)Math.cos(horizontalRotation))
+                .rotate(verticalRotation, (float)Math.sin(horizontalRotation), 0, -(float)Math.cos(horizontalRotation))
                 .translate(new Vector3f(scale).div(-2))
                 .scale(scale);
 
