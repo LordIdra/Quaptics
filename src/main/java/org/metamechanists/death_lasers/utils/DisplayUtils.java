424d7edfd72fde4f9082a404634bebac46c90c50
MetaMechanists <percy.baker.0@gmail.com>
2023-06-17 23:36:05 +0100

    argh

--- src/main/java/org/metamechanists/death_lasers/utils/DisplayUtils.java
+++ src/main/java/org/metamechanists/death_lasers/utils/DisplayUtils.java
@@ -72,7 +72,7 @@ public class DisplayUtils {
         return new Matrix4f()
                 //.translate(new Vector3f(scale).div(-2))
                 .scale(scale)
-                .lookAtLH(new Vector3f(0, 0, 0), new Vector3f(0, 0, 0).add(direction), UP_VECTOR);
+                .lookAt(new Vector3f(0, 0, 0), new Vector3f(0, 0, 0).add(direction), UP_VECTOR);
 
     }
 
