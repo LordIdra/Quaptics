ac69084cb56de76374a36122a2d915a0195ec3bc
MetaMechanists <percy.baker.0@gmail.com>
2023-06-17 21:53:31 +0100

    argh

--- src/main/java/org/metamechanists/death_lasers/utils/DisplayUtils.java
+++ src/main/java/org/metamechanists/death_lasers/utils/DisplayUtils.java
@@ -68,9 +68,9 @@ public class DisplayUtils {
         final float angle = new Vector3f(initial).angle(direction);
 
         return new Matrix4f()
-                .scale(scale)
+                .scale(scale);
                 //.rotate(angle, axis)
-                .translate(new Vector3f(scale).mul(-1));
+                //.translate(new Vector3f(scale).mul(-1));
     }
 
     public static Matrix4f simpleScaleTransformation(Vector3f scale) {
