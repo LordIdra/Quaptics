ffe2acf9ad6c73a5b03ef98d6c6cad28dcc44039
MetaMechanists <percy.baker.0@gmail.com>
2023-06-18 15:01:25 +0100

    scary maths continues

--- src/main/java/org/metamechanists/death_lasers/ScaryMathsTicker.java
+++ src/main/java/org/metamechanists/death_lasers/ScaryMathsTicker.java
@@ -3,6 +3,7 @@ package org.metamechanists.death_lasers;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.BlockDisplay;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.joml.Vector3f;
 import org.metamechanists.death_lasers.utils.DisplayUtils;
@@ -11,11 +12,17 @@ import java.util.UUID;
 
 public class ScaryMathsTicker extends BukkitRunnable {
 
+    BlockDisplay display = null;
+
     public void run() {
         final Location location = new Location(Bukkit.getServer().getWorld("world"), 872, 100, -270);
         final Location target = Bukkit.getServer().getPlayer(UUID.fromString("bb685dd0-8a71-4506-bc20-24d374fb28b4")).getLocation();
 
-        DisplayUtils.spawnBlockDisplay(location, Material.DISPENSER,
-                DisplayUtils.faceTargetTransformation(location, target, new Vector3f(1.0F, 1.0F, 1.0F)));
+        if (display == null) {
+            display = DisplayUtils.spawnBlockDisplay(location, Material.DISPENSER,
+                    DisplayUtils.faceTargetTransformation(location, target, new Vector3f(1.0F, 1.0F, 1.0F)));
+        } else {
+            display.setTransformationMatrix(DisplayUtils.faceTargetTransformation(location, target, new Vector3f(1.0F, 1.0F, 1.0F));
+        }
     }
 }
