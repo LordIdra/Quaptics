6465c3737aaae67778334635700fb630d1ac56d7
MetaMechanists <percy.baker.0@gmail.com>
2023-06-19 18:43:23 +0100

    Attempt to fix ticker spawn location

--- src/main/java/org/metamechanists/death_lasers/utils/DisplayUtils.java
+++ src/main/java/org/metamechanists/death_lasers/utils/DisplayUtils.java
@@ -47,7 +47,7 @@ public class DisplayUtils {
                 .mul(rotationYMatrix)
                 .mul(rotationXMatrix)
                 .scale(scale)
-                .translate(new Vector3f(scale).div(-2));
+                .translate(calculateHitboxAdjustmentTranslation(scale, new Vector3f(angleX, angleY, 0)));
     }
 
     public static Matrix4f simpleScaleTransformation(Vector3f scale) {
