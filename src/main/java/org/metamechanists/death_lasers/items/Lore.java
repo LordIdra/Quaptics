e7f377232f2dc3edfa2f2acba0d38779fbc7b288
MetaMechanists <percy.baker.0@gmail.com>
2023-06-20 21:45:39 +0100

    Amend component lore

--- src/main/java/org/metamechanists/death_lasers/items/Lore.java
+++ src/main/java/org/metamechanists/death_lasers/items/Lore.java
@@ -6,14 +6,14 @@ import java.util.Objects;
 
 public class Lore {
     public static String emissionPower(double emissionPower) {
-        return "&8⇨ &c⏻ " + Colors.EMISSION_POWER.getString() + "Emission Power &7" + Objects.toString(emissionPower) + "&8W";
+        return "&8⇨ " + Colors.EMISSION_POWER.getString() + "⏻ &7Emission Power &e" + Objects.toString(emissionPower) + " &8W";
     }
 
     public static String maxPower(double maxPower) {
-        return "&8⇨ &c⏻ " + Colors.MAX_POWER + "Max Power &7" + Objects.toString(maxPower) + "&8W";
+        return "&8⇨ " + Colors.MAX_POWER.getString() + "⏻ &7Max Power &e" + Objects.toString(maxPower) + " &8W";
     }
 
     public static String powerLoss(double powerLoss) {
-        return "&8⇨ &c⏻ " + Colors.POWER_LOSS + "&7Power Loss &7" + Objects.toString(powerLoss) + "&8W";
+        return "&8⇨ " + Colors.POWER_LOSS.getString() + "⏻ &7Power Loss &e" + Objects.toString(powerLoss) + " &8W";
     }
 }
