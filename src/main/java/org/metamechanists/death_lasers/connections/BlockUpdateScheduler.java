7da9573c46d134f30d99dd2b64cc42c48497230a
MetaMechanists <percy.baker.0@gmail.com>
2023-06-21 19:52:09 +0100

    Fix update propagation logic again

--- src/main/java/org/metamechanists/death_lasers/connections/BlockUpdateScheduler.java
+++ src/main/java/org/metamechanists/death_lasers/connections/BlockUpdateScheduler.java
@@ -7,7 +7,7 @@ public class BlockUpdateScheduler {
     // DANGER - this class is responsible for ensuring no update loops occur, which could crash the server and then make restarting it apallingly difficult
     // Think VERY carefully before modifying
 
-    private static final Queue<ConnectionGroup> groupsToUpdateNextTick = new ConcurrentLinkedQueue<>();
+    private static Queue<ConnectionGroup> newGroupsToTick = new ConcurrentLinkedQueue<>();
 
     private static void tickGroup(ConnectionGroup group) {
         // If group no longer exists, don't tick it
@@ -19,11 +19,15 @@ public class BlockUpdateScheduler {
     }
 
     public static void scheduleUpdate(ConnectionGroup group) {
-        groupsToUpdateNextTick.add(group);
+        newGroupsToTick.add(group);
     }
 
     public static void tick() {
-        groupsToUpdateNextTick.forEach(BlockUpdateScheduler::tickGroup);
-        groupsToUpdateNextTick.clear();
+        final Queue<ConnectionGroup> oldGroupsToTick = newGroupsToTick;
+        newGroupsToTick = new ConcurrentLinkedQueue<>();
+
+        while (!oldGroupsToTick.isEmpty()) {
+            tickGroup(oldGroupsToTick.remove());
+        }
     }
 }
