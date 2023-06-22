bcf9cbb9d9281f07e4d7be3090608a21c744ba1e
MetaMechanists <percy.baker.0@gmail.com>
2023-06-22 16:12:55 +0100

    Maybe fix storage death

--- src/main/java/org/metamechanists/death_lasers/implementation/tools/TargetingWand.java
+++ src/main/java/org/metamechanists/death_lasers/implementation/tools/TargetingWand.java
@@ -101,8 +101,9 @@ public class TargetingWand extends SlimefunItem {
             return;
         }
 
-        if (inputTargetPoint.hasLink()) { inputTargetPoint.getLink().remove(); }
-        if (outputSourcePoint.hasLink()) { outputSourcePoint.getLink().remove(); }
+        if (inputTargetPoint.hasLink()) {
+            inputTargetPoint.getLink().remove();
+        }
 
         block1.connect(sourcePoint, targetPoint);
         block2.connect(targetPoint, sourcePoint);
