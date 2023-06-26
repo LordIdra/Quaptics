90fac3906eb6f4b8c081592021b4d89807f5bc77
MetaMechanists <percy.baker.0@gmail.com>
2023-06-26 19:58:11 +0100

    debug

--- src/main/java/org/metamechanists/quaptics/QuapticTicker.java
+++ src/main/java/org/metamechanists/quaptics/QuapticTicker.java
@@ -3,15 +3,13 @@ package org.metamechanists.quaptics;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.metamechanists.quaptics.beams.DeprecatedTickerStorage;
 import org.metamechanists.quaptics.connections.BlockUpdateScheduler;
-import org.metamechanists.quaptics.connections.ConnectionGroup;
-import org.metamechanists.quaptics.storage.QuapticStorage;
 
 public class QuapticTicker extends BukkitRunnable {
     public static final int INTERVAl_TICKS = 1;
 
     @Override
     public void run() {
-        QuapticStorage.getLoadedGroups().forEach(ID -> ConnectionGroup.fromID(ID).tick());
+        //QuapticStorage.getLoadedGroups().forEach(ID -> ConnectionGroup.fromID(ID).tick());
         DeprecatedTickerStorage.tick();
         BlockUpdateScheduler.tick();
     }
