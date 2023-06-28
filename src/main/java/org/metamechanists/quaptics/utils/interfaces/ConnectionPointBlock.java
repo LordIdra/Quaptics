77dae6d75360369ce58ff2b4319b37f76ee5620e
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2023-06-27 21:05:11 -0500

    Undo Last Commit, do what we talked about

--- src/main/java/org/metamechanists/quaptics/utils/interfaces/ConnectionPointBlock.java
+++ src/main/java/org/metamechanists/quaptics/utils/interfaces/ConnectionPointBlock.java
@@ -1,26 +1,8 @@
 package org.metamechanists.quaptics.utils.interfaces;
 
-import io.github.thebusybiscuit.slimefun4.core.handlers.BlockUseHandler;
 import org.bukkit.block.Block;
-import org.metamechanists.quaptics.connections.points.ConnectionPoint;
 import org.metamechanists.quaptics.utils.id.ConnectionPointID;
 
 public interface ConnectionPointBlock {
     ConnectionPointID getPointId(Block block);
-
-    default BlockUseHandler onUse() {
-        return event -> {
-            final Block block = event.getClickedBlock().orElse(null);
-            if (block == null) {
-                return;
-            }
-
-            final ConnectionPoint connectionPoint = ConnectionPoint.fromID(getPointId(block));
-            if (connectionPoint == null) {
-                return;
-            }
-
-            connectionPoint.togglePanelVisibility();
-        };
-    }
 }
