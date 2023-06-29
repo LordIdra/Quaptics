02d48317f95bf4ecdaa013e3bb331158d1300968
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2023-06-29 16:25:02 -0500

    Whats gonna happen

--- src/main/java/org/metamechanists/quaptics/implementation/base/BurnoutRunnable.java
+++ src/main/java/org/metamechanists/quaptics/implementation/base/BurnoutRunnable.java
@@ -1,17 +1,25 @@
 package org.metamechanists.quaptics.implementation.base;
 
+import dev.sefiraat.sefilib.entity.display.DisplayGroup;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Display;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.metamechanists.quaptics.Quaptics;
+import org.metamechanists.quaptics.connections.ConnectionGroup;
+import org.metamechanists.quaptics.connections.points.ConnectionPoint;
+import org.metamechanists.quaptics.storage.QuapticStorage;
+import org.metamechanists.quaptics.utils.id.ConnectionGroupID;
 
 public class BurnoutRunnable extends BukkitRunnable {
+    private final ConnectionGroupID ID;
     private final Location location;
 
-    public BurnoutRunnable(Location location) {
+    public BurnoutRunnable(ConnectionGroupID ID, Location location) {
+        this.ID = ID;
         this.location = location;
     }
 
@@ -23,10 +31,18 @@ public class BurnoutRunnable extends BukkitRunnable {
         }
 
         for (int delay = 1; delay < 60; delay++) {
-            Bukkit.getScheduler().runTaskLater(
-                    Quaptics.getInstance(),
-                    () -> location.getWorld().spawnParticle(Particle.LAVA, location.clone().add(ConnectedBlock.RELATIVE_CENTER), 1),
-                    delay);
+            Bukkit.getScheduler().runTaskLater(Quaptics.getInstance(), () -> {
+                final DisplayGroup group = connectedBlock.getDisplayGroup(location);
+                if (group != null) {
+                    group.getDisplays().values().forEach(display -> {
+                        final Display.Brightness brightness = display.getBrightness();
+                        display.setBrightness(new Display.Brightness(
+                                brightness == null ? 14 : Math.min(0, brightness.getBlockLight() - 1),
+                                brightness == null ? 14 : Math.min(0, brightness.getSkyLight() - 1)));
+                    });
+                    location.getWorld().spawnParticle(Particle.LAVA, location.clone().add(ConnectedBlock.RELATIVE_CENTER), 1);
+                }
+            }, delay);
         }
 
         Bukkit.getScheduler().runTaskLater(Quaptics.getInstance(), () -> connectedBlock.burnout(block.getLocation()), 60L);
