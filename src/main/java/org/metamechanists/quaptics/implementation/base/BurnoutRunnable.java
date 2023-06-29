0ef6e2dc9be130cd3a3b3f21c47f621f7fde3caa
JustAHuman-xD <65748158+JustAHuman-xD@users.noreply.github.com>
2023-06-29 16:37:01 -0500

    Some more adjustments to burnout

--- src/main/java/org/metamechanists/quaptics/implementation/base/BurnoutRunnable.java
+++ src/main/java/org/metamechanists/quaptics/implementation/base/BurnoutRunnable.java
@@ -1,5 +1,6 @@
 package org.metamechanists.quaptics.implementation.base;
 
+import com.destroystokyo.paper.ParticleBuilder;
 import dev.sefiraat.sefilib.entity.display.DisplayGroup;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import org.bukkit.Bukkit;
@@ -25,16 +26,20 @@ public class BurnoutRunnable extends BukkitRunnable {
         }
 
         for (int delay = 1; delay < 60; delay++) {
+            final int ticks = delay;
             Bukkit.getScheduler().runTaskLater(Quaptics.getInstance(), () -> {
                 final DisplayGroup group = connectedBlock.getDisplayGroup(location.clone());
-                if (group != null) {
+                if (group != null && ticks % 4 == 0) {
                     group.getDisplays().values().forEach(display -> {
                         final Display.Brightness brightness = display.getBrightness();
                         display.setBrightness(new Display.Brightness(
                                 brightness == null ? 14 : Math.max(0, brightness.getBlockLight() - 1),
                                 brightness == null ? 14 : Math.max(0, brightness.getSkyLight() - 1)));
                     });
-                    location.clone().getWorld().spawnParticle(Particle.LAVA, location.clone().add(ConnectedBlock.RELATIVE_CENTER), 1);
+
+                    new ParticleBuilder(Particle.LAVA)
+                            .location(location.clone().add(ConnectedBlock.RELATIVE_CENTER))
+                            .spawn();
                 }
             }, delay);
         }
