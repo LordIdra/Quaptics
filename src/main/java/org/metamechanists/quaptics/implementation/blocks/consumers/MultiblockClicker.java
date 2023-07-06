3eb67a763c2205ff76d59331976525a77195b712
MetaMechanists <percy.baker.0@gmail.com>
2023-07-06 15:29:26 +0100

    Add SlimefunIsDumbUtils

--- src/main/java/org/metamechanists/quaptics/implementation/blocks/consumers/MultiblockClicker.java
+++ src/main/java/org/metamechanists/quaptics/implementation/blocks/consumers/MultiblockClicker.java
@@ -4,7 +4,6 @@ import dev.sefiraat.sefilib.entity.display.DisplayGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
-import io.github.thebusybiscuit.slimefun4.core.multiblocks.MultiBlockMachine;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
@@ -13,6 +12,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 import org.joml.Vector3f;
 import org.metamechanists.quaptics.connections.ConnectionGroup;
@@ -20,6 +20,7 @@ import org.metamechanists.quaptics.connections.ConnectionPoint;
 import org.metamechanists.quaptics.implementation.blocks.Settings;
 import org.metamechanists.quaptics.implementation.blocks.base.ConnectedBlock;
 import org.metamechanists.quaptics.utils.Keys;
+import org.metamechanists.quaptics.utils.SlimefunIsDumbUtils;
 import org.metamechanists.quaptics.utils.Transformations;
 import org.metamechanists.quaptics.utils.builders.BlockDisplayBuilder;
 import org.metamechanists.quaptics.utils.id.complex.ConnectionGroupId;
@@ -30,7 +31,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class MultiblockClicker extends ConnectedBlock {
-    private static final float PLATE_DISTANCE = 0.5F;
+    private static final Vector RELATIVE_PLATE_LOCATION = new Vector(0, 0, 0.5F);
     private final Vector3f attachmentDisplaySize = new Vector3f(0.1F, 0.7F, 0.7F);
     private final Vector3f mainDisplaySize = new Vector3f(0.3F, 0.3F, 0.3F);
 
@@ -45,9 +46,9 @@ public class MultiblockClicker extends ConnectedBlock {
                 .setBlockData(Material.LIGHT_GRAY_CONCRETE.createBlockData())
                 .setTransformation(Transformations.adjustedScale(mainDisplaySize))
                 .build());
-        displayGroup.addDisplay("attachment", new BlockDisplayBuilder(location.toCenterLocation())
+        displayGroup.addDisplay("attachment", new BlockDisplayBuilder(formatPointLocation(player, location.toCenterLocation(), RELATIVE_PLATE_LOCATION))
                 .setBlockData(Material.WHITE_CONCRETE.createBlockData())
-                .setTransformation(Transformations.adjustedScaleAndOffset(attachmentDisplaySize, player.getFacing().getDirection().toVector3f().mul(PLATE_DISTANCE)))
+                .setTransformation(Transformations.adjustedScale(attachmentDisplaySize))
                 .build());
         BlockStorage.addBlockInfo(location, Keys.BS_OWNER, player.getUniqueId().toString());
         BlockStorage.addBlockInfo(location, Keys.BS_FACING, player.getFacing().toString());
@@ -73,15 +74,13 @@ public class MultiblockClicker extends ConnectedBlock {
 
         final BlockFace face = BlockFace.valueOf(BlockStorage.getLocationInfo(location.get(), Keys.BS_FACING));
         final Block multiblockBlock = location.get().getBlock().getRelative(face);
-        if (!(BlockStorage.check(multiblockBlock) instanceof final MultiBlockMachine multiblock)) {
-            return;
-        }
-
-        multiblock.onInteract(owner, multiblockBlock);
+        SlimefunIsDumbUtils.getMultiblockMachine(multiblockBlock).ifPresent(block -> block.onInteract(owner, multiblockBlock));
     }
 
     @Override
     public void onInputLinkUpdated(@NotNull final ConnectionGroup group) {
         //doBurnoutCheck(group, "input");
     }
+
+
 }
---
+++ src/main/java/org/metamechanists/quaptics/utils/SlimefunIsDumbUtils.java
@@ -0,0 +1,61 @@
+package org.metamechanists.quaptics.utils;
+
+import io.github.thebusybiscuit.slimefun4.core.multiblocks.MultiBlock;
+import io.github.thebusybiscuit.slimefun4.core.multiblocks.MultiBlockMachine;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import lombok.experimental.UtilityClass;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Optional;
+
+@UtilityClass
+public class SlimefunIsDumbUtils {
+    // https://github.com/Slimefun/Slimefun4/blob/master/src/main/java/io/github/thebusybiscuit/slimefun4/implementation/listeners/MultiBlockListener.java#L65
+    public Optional<MultiBlockMachine> getMultiblockMachine(final Block block) {
+        final LinkedList<MultiBlock> multiblocks = new LinkedList<>();
+
+        for (final MultiBlock mb : Slimefun.getRegistry().getMultiBlocks()) {
+            final Block center = block.getRelative(mb.getTriggerBlock());
+            if (compareMaterials(center, mb.getStructure(), mb.isSymmetric())) {
+                multiblocks.add(mb);
+            }
+        }
+
+        return multiblocks.isEmpty()
+                ? Optional.empty()
+                : Optional.of((MultiBlockMachine) multiblocks.getLast().getSlimefunItem());
+
+    }
+
+    @ParametersAreNonnullByDefault
+    private boolean compareMaterials(final Block block, final Material[] blocks, final boolean onlyTwoWay) {
+        if (!compareMaterialsVertical(block, blocks[1], blocks[4], blocks[7])) {
+            return false;
+        }
+
+        final BlockFace[] directions = onlyTwoWay
+                ? new BlockFace[] { BlockFace.NORTH, BlockFace.EAST }
+                : new BlockFace[] { BlockFace.NORTH, BlockFace.EAST, BlockFace.SOUTH, BlockFace.WEST };
+
+        return Arrays.stream(directions).anyMatch(direction ->
+                compareMaterialsVertical(block.getRelative(direction), blocks[0], blocks[3], blocks[6])
+                        && compareMaterialsVertical(block.getRelative(direction.getOppositeFace()), blocks[2], blocks[5], blocks[8]));
+    }
+
+    private boolean compareMaterialsVertical(final Block block, @Nullable final Material top, @Nullable final Material center, @Nullable final Material bottom) {
+        return (center == null || equals(block.getType(), center))
+                && (top == null || equals(block.getRelative(BlockFace.UP).getType(), top))
+                && (bottom == null || equals(block.getRelative(BlockFace.DOWN).getType(), bottom));
+    }
+
+    @ParametersAreNonnullByDefault
+    private boolean equals(final Material a, final Material b) {
+        return a == b || MultiBlock.getSupportedTags().stream().anyMatch(tag -> tag.isTagged(a) && tag.isTagged(b));
+    }
+}
