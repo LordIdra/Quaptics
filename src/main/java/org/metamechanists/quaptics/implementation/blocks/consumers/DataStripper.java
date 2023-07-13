0fcc6d7d12bbb26cf00ce62d105cce35d6935105
MetaMechanists <percy.baker.0@gmail.com>
2023-07-13 17:31:51 +0100

    Model work

--- src/main/java/org/metamechanists/quaptics/implementation/blocks/consumers/DataStripper.java
+++ src/main/java/org/metamechanists/quaptics/implementation/blocks/consumers/DataStripper.java
@@ -86,7 +86,7 @@ public class DataStripper extends ConnectedBlock implements InfoPanelBlock, Item
                         .location(0, -0.35F, 0)
                         .size(0.3F, 0.15F, 0.3F))
                 .add("item", new ModelItem()
-                        .size(0.5F))
+                        .size(1.0F))
                 .build(location);
     }
     @Override
