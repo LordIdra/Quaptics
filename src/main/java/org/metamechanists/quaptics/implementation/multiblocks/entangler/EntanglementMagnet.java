a280309b78aa861f18148456f1e6f28c2141e034
MetaMechanists <percy.baker.0@gmail.com>
2023-07-14 15:25:07 +0100

    Entangler visuals

--- src/main/java/org/metamechanists/quaptics/implementation/multiblocks/entangler/EntanglementMagnet.java
+++ src/main/java/org/metamechanists/quaptics/implementation/multiblocks/entangler/EntanglementMagnet.java
@@ -10,8 +10,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
-import org.joml.Vector3d;
-import org.joml.Vector3f;
 import org.metamechanists.quaptics.connections.ConnectionGroup;
 import org.metamechanists.quaptics.connections.ConnectionPoint;
 import org.metamechanists.quaptics.connections.ConnectionPointType;
@@ -24,9 +22,10 @@ import org.metamechanists.quaptics.items.Tier;
 import org.metamechanists.quaptics.utils.BlockStorageAPI;
 import org.metamechanists.quaptics.utils.Keys;
 import org.metamechanists.quaptics.utils.Utils;
-import org.metamechanists.quaptics.utils.builders.BlockDisplayBuilder;
 import org.metamechanists.quaptics.utils.id.complex.ConnectionGroupId;
-import org.metamechanists.quaptics.utils.transformations.TransformationMatrixBuilder;
+import org.metamechanists.quaptics.utils.models.ModelBuilder;
+import org.metamechanists.quaptics.utils.models.components.ModelCuboid;
+import org.metamechanists.quaptics.utils.models.components.ModelDiamond;
 
 import java.util.List;
 import java.util.Optional;
@@ -44,25 +43,6 @@ public class EntanglementMagnet extends ConnectedBlock implements PowerAnimatedB
             Lore.create(ENTANGLEMENT_MAGNET_SETTINGS,
                     "&7● Multiblock component"));
 
-    private static final Vector3f BASE_SCALE = new Vector3f(0.80F, 0.40F, 0.80F);
-    private static final Vector3f BASE_OFFSET = new Vector3f(0.0F, 0.0F, 0.0F);
-    private static final Vector3d BASE_ROTATION = new Vector3d(0.0F, Math.PI/4, 0.0F);
-    private static final Vector3f COIL_1_SCALE = new Vector3f(0.60F, 0.20F, 0.10F);
-    private static final Vector3f COIL_2_SCALE = new Vector3f(0.10F, 0.20F, 0.60F);
-    private static final Vector3f COIL_3_SCALE = new Vector3f(0.10F, 0.20F, 0.60F);
-    private static final Vector3f COIL_4_SCALE = new Vector3f(0.60F, 0.20F, 0.10F);
-    private static final Vector3d COIL_ROTATION = new Vector3d(0.0F, Math.PI/4, 0.0F);
-    private static final Vector3f COIL_1_OFFSET = new Vector3f(0.30F, 0.0F, 0.30F);
-    private static final Vector3f COIL_2_OFFSET = new Vector3f(0.30F, 0.0F, -0.30F);
-    private static final Vector3f COIL_3_OFFSET = new Vector3f(-0.30F, 0.0F, 0.30F);
-    private static final Vector3f COIL_4_OFFSET = new Vector3f(-0.30F, 0.0F, -0.30F);
-    private static final Vector3f BOTTOM_PLATE_SCALE = new Vector3f(0.50F, 0.10F, 0.50F);
-    private static final Vector3f BOTTOM_PLATE_OFFSET = new Vector3f(0.0F, 0.20F, 0.0F);
-    private static final Vector3d BOTTOM_PLATE_ROTATION = new Vector3d(0.0F, Math.PI/4, 0.0F);
-    private static final Vector3f TOP_PLATE_SCALE = new Vector3f(0.50F, 0.10F, 0.50F);
-    private static final Vector3f TOP_PLATE_OFFSET = new Vector3f(0.0F, -0.20F, 0.0F);
-    private static final Vector3d TOP_PLATE_ROTATION = new Vector3d(0.0F, Math.PI/4, 0.0F);
-
     private final Vector inputPointLocation = new Vector(0.0F, 0.0F, -getConnectionRadius());
 
     public EntanglementMagnet(final ItemGroup itemGroup, final SlimefunItemStack item, final RecipeType recipeType, final ItemStack[] recipe, final Settings settings) {
@@ -75,68 +55,23 @@ public class EntanglementMagnet extends ConnectedBlock implements PowerAnimatedB
     }
     @Override
     protected DisplayGroup initModel(final @NotNull Location location, final @NotNull Player player) {
-        final DisplayGroup displayGroup = new DisplayGroup(location);
-        displayGroup.addDisplay("base", new BlockDisplayBuilder()
-                .blockData(Material.GRAY_CONCRETE.createBlockData())
-                .transformation(new TransformationMatrixBuilder()
-                        .scale(BASE_SCALE)
-                        .rotate(BASE_ROTATION)
-                        .translate(BASE_OFFSET)
-                        .buildForBlockDisplay())
-                .build(location.toCenterLocation()));
-        displayGroup.addDisplay("bottom_plate", new BlockDisplayBuilder()
-                .blockData(Material.LIGHT_GRAY_CONCRETE.createBlockData())
-                .transformation(new TransformationMatrixBuilder()
-                        .scale(BOTTOM_PLATE_SCALE)
-                        .rotate(BOTTOM_PLATE_ROTATION)
-                        .translate(BOTTOM_PLATE_OFFSET)
-                        .buildForBlockDisplay())
-                .build(location.toCenterLocation()));
-        displayGroup.addDisplay("top_plate", new BlockDisplayBuilder()
-                .blockData(Material.LIGHT_GRAY_CONCRETE.createBlockData())
-                .transformation(new TransformationMatrixBuilder()
-                        .scale(TOP_PLATE_SCALE)
-                        .rotate(TOP_PLATE_ROTATION)
-                        .translate(TOP_PLATE_OFFSET)
-                        .buildForBlockDisplay())
-                .build(location.toCenterLocation()));
-        displayGroup.addDisplay("coil1", new BlockDisplayBuilder()
-                .blockData(Material.ORANGE_CONCRETE.createBlockData())
-                .brightness(Utils.BRIGHTNESS_OFF)
-                .transformation(new TransformationMatrixBuilder()
-                        .scale(COIL_1_SCALE)
-                        .rotate(COIL_ROTATION)
-                        .translate(COIL_1_OFFSET)
-                        .buildForBlockDisplay())
-                .build(location.toCenterLocation()));
-        displayGroup.addDisplay("coil2", new BlockDisplayBuilder()
-                .blockData(Material.ORANGE_CONCRETE.createBlockData())
-                .brightness(Utils.BRIGHTNESS_OFF)
-                .transformation(new TransformationMatrixBuilder()
-                        .scale(COIL_2_SCALE)
-                        .rotate(COIL_ROTATION)
-                        .translate(COIL_2_OFFSET)
-                        .buildForBlockDisplay())
-                .build(location.toCenterLocation()));
-        displayGroup.addDisplay("coil3", new BlockDisplayBuilder()
-                .blockData(Material.ORANGE_CONCRETE.createBlockData())
-                .brightness(Utils.BRIGHTNESS_OFF)
-                .transformation(new TransformationMatrixBuilder()
-                        .scale(COIL_3_SCALE)
-                        .rotate(COIL_ROTATION)
-                        .translate(COIL_3_OFFSET)
-                        .buildForBlockDisplay())
-                .build(location.toCenterLocation()));
-        displayGroup.addDisplay("coil4", new BlockDisplayBuilder()
-                .blockData(Material.ORANGE_CONCRETE.createBlockData())
-                .brightness(Utils.BRIGHTNESS_OFF)
-                .transformation(new TransformationMatrixBuilder()
-                        .scale(COIL_4_SCALE)
-                        .rotate(COIL_ROTATION)
-                        .translate(COIL_4_OFFSET)
-                        .buildForBlockDisplay())
-                .build(location.toCenterLocation()));
-        return displayGroup;
+        return new ModelBuilder()
+                .add("diamond", new ModelDiamond()
+                        .material(Material.GRAY_CONCRETE)
+                        .size(0.5F))
+                .add("panel1", new ModelCuboid()
+                        .material(Material.ORANGE_CONCRETE)
+                        .brightness(Utils.BRIGHTNESS_OFF)
+                        .size(0.6F, 0.3F, 0.3F))
+                .add("panel2", new ModelCuboid()
+                        .material(Material.ORANGE_CONCRETE)
+                        .brightness(Utils.BRIGHTNESS_OFF)
+                        .size(0.3F, 0.6F, 0.3F))
+                .add("panel3", new ModelCuboid()
+                        .material(Material.ORANGE_CONCRETE)
+                        .brightness(Utils.BRIGHTNESS_OFF)
+                        .size(0.3F, 0.3F, 0.6F))
+                .buildAtBlockCenter(location);
     }
     @Override
     protected List<ConnectionPoint> initConnectionPoints(final ConnectionGroupId groupId, final Player player, final Location location) {
@@ -159,9 +94,8 @@ public class EntanglementMagnet extends ConnectedBlock implements PowerAnimatedB
     }
     @Override
     public void onPoweredAnimation(final Location location, final boolean powered) {
-        brightnessAnimation(location, "coil1", powered);
-        brightnessAnimation(location, "coil2", powered);
-        brightnessAnimation(location, "coil3", powered);
-        brightnessAnimation(location, "coil4", powered);
+        brightnessAnimation(location, "panel1", powered);
+        brightnessAnimation(location, "panel2", powered);
+        brightnessAnimation(location, "panel3", powered);
     }
 }
