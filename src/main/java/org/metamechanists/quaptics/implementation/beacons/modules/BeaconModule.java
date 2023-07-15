66b4b1eab269196403b8a75b149e6e90aa25d314
MetaMechanists <percy.baker.0@gmail.com>
2023-07-15 16:43:41 +0100

    Make modules not placeable

--- src/main/java/org/metamechanists/quaptics/implementation/beacons/modules/BeaconModule.java
+++ src/main/java/org/metamechanists/quaptics/implementation/beacons/modules/BeaconModule.java
@@ -1,14 +1,14 @@
 package org.metamechanists.quaptics.implementation.beacons.modules;
 
 import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup;
-import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
+import io.github.thebusybiscuit.slimefun4.implementation.items.blocks.UnplaceableBlock;
 import org.bukkit.inventory.ItemStack;
 import org.metamechanists.quaptics.implementation.blocks.Settings;
 
 
-public abstract class BeaconModule extends SlimefunItem {
+public abstract class BeaconModule extends UnplaceableBlock {
     protected final Settings settings;
 
     protected BeaconModule(final ItemGroup itemGroup, final SlimefunItemStack item, final RecipeType recipeType, final ItemStack[] recipe, final Settings settings) {
